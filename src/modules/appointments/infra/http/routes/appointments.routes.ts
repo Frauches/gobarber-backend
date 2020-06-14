66b4bd033616e9cdfae138805d75b31ce5cf0d8a
Matheus Frauches <frauches.27@gmail.com>
2020-06-14 20:42:08 -0300

    Apply dependency inversion to the Appointments Service and Repository

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,26 +1,28 @@
 import { parseISO } from 'date-fns';
 import { Request, Response, Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 const appointmentRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentRouter.use(ensureAuthenticated);
 
-appointmentRouter.get('/', async (request: Request, response: Response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+// appointmentRouter.get('/', async (request: Request, response: Response) => {
+//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentRouter.post('/', async (request: Request, response: Response) => {
   const { provider_id, date } = request.body;
   const parsedDate = parseISO(date);
 
-  const createAppointmentService = new CreateAppointmentService();
+  const createAppointmentService = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointmentService.execute({
     provider_id,
--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,13 +1,29 @@
-import { EntityRepository, Repository } from 'typeorm';
-import Appointment from '../entities/Appointment';
+import { Repository, getRepository } from 'typeorm';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import Appointment from '../entities/Appointment';
+import ICreateAppointmentDTO from '../../../dtos/ICreateAppointmentDTO';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> implements IAppointmentsRepository {
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({ where: { date } });
+    const findAppointment = await this.ormRepository.findOne({
+      where: { date },
+    });
 
     return findAppointment;
+  }
+
+  public async create({ provider_id, date }: ICreateAppointmentDTO) {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
