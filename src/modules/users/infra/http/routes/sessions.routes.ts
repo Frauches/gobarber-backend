0a0d3090a6a4e5ea0258cbc8a0975a2b67ee8cd0
Matheus Frauches <frauches.27@gmail.com>
2020-06-17 23:23:36 -0300

    Create SessionsController and apply it to the sessions routes

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,19 +1,9 @@
-import { Request, Response, Router } from 'express';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import { container } from 'tsyringe';
+import { Router } from 'express';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request: Request, response: Response) => {
-  const { email, password } = request.body;
-
-  const createSession = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await createSession.execute({ email, password });
-
-  delete user.password;
-
-  return response.json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
