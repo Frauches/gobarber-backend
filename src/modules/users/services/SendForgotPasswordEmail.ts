7efa0f209c333508641f3971f8102af404a2707d
Matheus Frauches <frauches.27@gmail.com>
2020-08-20 08:00:07 -0300

    Apply MailTemplateProvider to receive a file as template data

--- src/modules/users/services/SendForgotPasswordEmail.ts
+++ src/modules/users/services/SendForgotPasswordEmail.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IMailProvider from '../../../shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '../../../shared/errors/AppError';
@@ -30,6 +31,13 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -37,7 +45,7 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Recuperação de Senha',
       templateData: {
-        template: 'Olá, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
           token,
